f8d683447c8f1884d7fd9f80150ded1ea5761609
sohelmulla146200-lab <sohelmulla146200@gmail.com>
2026-03-30 16:04:33 +0530

    Create activity 2.py

---
+++ exp 8/activity 2.py
@@ -0,0 +1,38 @@
+# ATM withdraw system handles insufficient balance.
+"""
+Created on Mon Mar 30 15:35:42 2026
+
+@author: sohel mulla
+
+class InsufficientBalanceError(Exception):
+  """Custom Exception for insufficient balance"""
+pass
+
+def withdraw(balance, amount):
+ try:
+  if amount > balance:
+   raise InsufficientBalanceError("Insufficient balance in your account.")
+  elif amount <= 0:
+    raise ValueError("Enter a valid withdrawal amount.")
+  else:
+   balance -= amount
+  print(f"Withdrawal successful! Remaining balance: ₹{balance}")
+  return balance
+ except InsufficientBalanceError as e:
+  print("Error:", e)
+
+ except ValueError as v:
+  print("Error:", v)
+
+ except Exception as ex:
+  print("Unexpected error:", ex)
+
+# Main program
+balance = 5000 # Initial balance
+
+try:
+ amount = float(input("Enter amount to withdraw: ₹"))
+ balance = withdraw(balance, amount)
+except Exception as e:
+ print("Invalid input:", e)
+
